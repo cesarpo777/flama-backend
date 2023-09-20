6d68796f9d2a8f531b903911e61b2fe8d0f63e45
cesar muzio <cmuzio375@gmail.com>
2023-09-20 00:57:50 -0300

    add register user route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,25 @@
 const { Router } = require("express");
 // const { validarJWT } = require('../middlewares/validarJwt')
-// const { check } = require('express-validator');
+const { check } = require('express-validator');
 const router = Router();
-const { getAll } = require("../controllers/user.controller");
-// const { validarCampos } = require('../middlewares/validar-campos');
+const { getAll, createUser } = require("../controllers/user.controller");
+const { validarCampos } = require('../middlewares/validar-campos');
 
-// obtener pizzas
+
 router.get("/", getAll);
+
+router.post(
+    "/register",
+    [
+      check("name", "El campo nombre es obligatorio").not().isEmpty(),
+      check("correo", "Introduce un correo vàlido").isEmail(),
+      check(
+        "password",
+        "La contraseña debe tener al menos 6 caracteres"
+      ).isLength({ min: 6 }),
+      validarCampos,
+    ],
+    createUser
+  );
 
 module.exports = router;
